1a5a9a3f7effcd054a7f11a38578d372d5ac8887
ryo.takahashi <b152029@kca.ac.jp>
2018-01-02 17:28:56 +0900

    [add] #00 最小機能実装

--- MyCamera/ViewController.swift
+++ MyCamera/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UINavigationControllerDelegate,UIImagePickerControllerDelegate{
 
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func launchCamera(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            print("[INFO] カメラ使用可能")
+            let imagePickerController = UIImagePickerController()
+            imagePickerController.sourceType = .camera
+            imagePickerController.delegate = self
+            present(imagePickerController, animated: true, completion: nil)
+        } else {
+            print("[ERROR] カメラ使用不可能")
+        }
+    }
+    
+    @IBAction func launchShareWindow(_ sender: Any) {
+        if let sharedImage = imageView.image {
+            let sharedItems = [sharedImage]
+            let controller = UIActivityViewController(activityItems: sharedItems,applicationActivities: nil)
+            controller.popoverPresentationController?.sourceView = view
+            present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
 }
 
